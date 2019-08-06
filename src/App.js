341512d53efa5dae8584f492eaeaa74dec5a44ae
Reuben Ayres <ra@sent.com>
2019-08-06 13:07:49 -0500

    you can log out

--- src/App.js
+++ src/App.js
@@ -8,8 +8,12 @@ class App extends React.Component {
     super() 
 
     this.state = {
+      // this is the single source of truth about whether and who is logged in
+      /// you should NOT store the same data in multiple places
+      // store it as high up as it needs to be to be able to be passed down
+      // to the necessary children
       loggedIn: false,
-      username: ''
+      username: '' 
     }
 
   }
@@ -25,6 +29,15 @@ class App extends React.Component {
     })
   }
 
+  /// LOGOUT FUNCTION HERE-- because this component is the only one 
+  // that actually knows whether we're logged in
+  // be sure to pass this function in props down to MainContainer
+  logout = () => {
+    this.setState({
+      loggedIn: false,
+      username: ''
+    })
+  }
 
   render() {
     console.log("this.state in render() in App:");
@@ -33,7 +46,7 @@ class App extends React.Component {
       <div className="App">
         <h1>React Forms</h1>
         { this.state.loggedIn ? null : <Login login={this.login} /> }
-        { this.state.loggedIn ? <MainContainer username={this.state.username}/> : null }
+        { this.state.loggedIn ? <MainContainer logout={this.logout} username={this.state.username} /> : null }
       </div>
     );
   }
--- src/MainContainer/index.js
+++ src/MainContainer/index.js
@@ -5,11 +5,16 @@
 import React from 'react'
 
 function MainContainer(props) {
+
+  // render a button below the div after the h3 that logs 
+  // the user out by calling the logout function from props
+
   console.log("here is props in MainContainer");
   console.log(props);
   return(
     <div>
       <h3>Hello you are logged in as {props.username}</h3>
+      <button onClick={props.logout}> Logout </button>
     </div>
   )
 
