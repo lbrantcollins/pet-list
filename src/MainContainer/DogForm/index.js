defb87581de4559253260d6e0eb640ebc8fc3ff9
Reuben Ayres <ra@sent.com>
2019-08-06 15:27:15 -0500

    You can type in the form

--- src/MainContainer/DogForm/index.js
+++ src/MainContainer/DogForm/index.js
@@ -9,20 +9,26 @@ class DogForm extends React.Component {
       breed: ''
     }
   }
-
+  handleChange = (event) => {
+    this.setState({
+      [event.currentTarget.name]: event.currentTarget.value
+    })
+  }
   render() {
+    console.log("this.state in render() in DogForm");
+    console.log(this.state);
     return(
       <div>
         <h3>Add your dogs here, folks!</h3>
         <form>
-          <input type="text" name="name" value={this.state.name} placeholder="Enter a name" />
+          <input type="text" name="name" value={this.state.name} placeholder="Enter a name" onChange={this.handleChange} />
           <br />
-          <input type="number" name="age" value={this.state.age} placeholder="Enter age" />
+          <input type="number" name="age" value={this.state.age} placeholder="Enter age" onChange={this.handleChange} />
           <br />
-          <input type="text" name="breed" value={this.state.breed} placeholder="Enter a breed" />
+          <input type="text" name="breed" value={this.state.breed} placeholder="Enter a breed" onChange={this.handleChange} />
           <br />
 
-
+          <button>Add Dog</button> 
         </form>
 
       </div>
