55e082236752d8cba4f6dd3e6db5a580a048f4ae
Reuben Ayres <ra@sent.com>
2019-08-06 11:57:53 -0500

    you can type username and it is updated in state

--- src/Login/index.js
+++ src/Login/index.js
@@ -13,17 +13,39 @@ class Login extends React.Component {
       username: '', // set initial values here
       password: ''
     }
-    
+  }
+
+  handleUsernameChange = (event) => {
+    console.log("handleUsernameChange");
+    console.log(event.currentTarget.value);
+    this.setState({
+      username: event.currentTarget.value
+    })
+    /// this is not a good place to use console.log to check something in state
   }
 
   render() {
+    // this is a good place to check that something is in state
+    console.log("this.state in render() in Login");
+    console.log(this.state);
     return(
       <div>
         <h2>Login</h2>
         <form>
-          <input type="text" name="username" value={this.state.username} placeholder="username" />
+          <input 
+            type="text" 
+            name="username" 
+            value={this.state.username} 
+            placeholder="username"
+            onChange={this.handleUsernameChange}
+          />
           <br />
-          <input type="password" name="password" value={this.state.password} placeholder="password" />
+          <input 
+            type="password" 
+            name="password" 
+            value={this.state.password} 
+            placeholder="password" 
+          />
           <br />
           <button>Log in!</button>
         </form>
