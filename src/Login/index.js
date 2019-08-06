1144db9fda4bca1fe516af9cace14ce4dcd082b4
Reuben Ayres <ra@sent.com>
2019-08-06 11:47:21 -0500

    form values linked to state -- cannot type in form -- need onChange

--- src/Login/index.js
+++ src/Login/index.js
@@ -1,15 +1,29 @@
 import React from 'react';
 
+
+// This is a container component because it is keeping track
+// of some data in state
+
 class Login extends React.Component {
+
+  constructor() {
+    super() // calling the constructor for React.Component
+
+    this.state = {
+      username: '', // set initial values here
+      password: ''
+    }
+    
+  }
 
   render() {
     return(
       <div>
         <h2>Login</h2>
         <form>
-          <input type="text" name="username" placeholder="username" />
+          <input type="text" name="username" value={this.state.username} placeholder="username" />
           <br />
-          <input type="password" name="password" placeholder="password" />
+          <input type="password" name="password" value={this.state.password} placeholder="password" />
           <br />
           <button>Log in!</button>
         </form>
