13905b279b44dbabebd2ed25deae0d81fd6328ab
lbrantcollins <lbrantcollins@gmail.com>
2019-08-08 22:43:14 -0500

    improved select list functionality for deleting dogs

--- src/MainContainer/SelectDelete/index.js
+++ src/MainContainer/SelectDelete/index.js
@@ -8,14 +8,23 @@ function SelectDelete(props) {
 
 	if (props.dogs.length > 0) {
 
-		const dogOptions = props.dogs.map( (dog, i) => {
-			return <option key={i}>{dog.name}</option>;
+		const dogs = [...props.dogs];
+		dogs.unshift({name: "--Choose one dog to delete--"});
+		console.log(dogs);
+
+		const dogOptions = dogs.map( (dog, i) => {
+			return (
+				<option key={i}>{dog.name}</option>
+			)
 		})
  		
+ 	// 	console.log("dogOptions:")
+		// console.log(dogOptions)
+
 		return (
 		  	<div>
 		  		<p>Choose one dog to delete:</p>
-			  	<select size={props.dogs.length} onClick={handleSelect}> {dogOptions} </select>
+			  	<select size={props.dogs.length+1} onClick={handleSelect}> {dogOptions} </select>
 		  	</div>
 		 );
 	} else {
